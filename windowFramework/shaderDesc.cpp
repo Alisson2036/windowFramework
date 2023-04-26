e46c23a582a5b4e17c0af2450d29c8e4bab52fda
Alisson Parizoto <alissonparizoto@hotmail.com>
2023-04-26 20:36:54 -0300

    Criando a classe ShaderDesc, para analizar os shaders

---
+++ windowFramework/shaderDesc.cpp
@@ -0,0 +1,41 @@
+#include "shaderDesc.h"
+
+ShaderDesc::ShaderDesc(Microsoft::WRL::ComPtr<ID3DBlob> blob)
+{
+	//criando o reflect do shader
+	Microsoft::WRL::ComPtr<ID3D11ShaderReflection> reflect;
+
+	//refletindo o shader
+	_throwHr(
+		D3DReflect(blob->GetBufferPointer(),
+			blob->GetBufferSize(),
+			__uuidof(ID3D11ShaderReflection),
+			(void**)reflect.GetAddressOf()
+		)
+	);
+
+	//pegando a descricao do shader
+	D3D11_SHADER_DESC desc = {};
+	reflect->GetDesc(&desc);
+
+	//lendo todos os constantBuffers do shader
+	for (int i = 0; i < desc.ConstantBuffers; i++)
+	{
+		ID3D11ShaderReflectionConstantBuffer* buf = reflect->GetConstantBufferByIndex(i);
+		D3D11_SHADER_BUFFER_DESC bufDesc = {};
+		buf->GetDesc(&bufDesc);
+		ID3D11ShaderReflectionVariable* var = buf->GetVariableByIndex(0);
+
+
+	}
+
+	//lendo inputLayout
+	for (int i = 0; i < desc.InputParameters; i++)
+	{
+		D3D11_SIGNATURE_PARAMETER_DESC parDesc;
+		reflect->GetInputParameterDesc(i, &parDesc);
+		inputParams.push_back((std::string)parDesc.SemanticName);
+
+	}
+
+}
---
+++ windowFramework/shaderDesc.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <wrl.h>
+#include <Windows.h>
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <string>
+#include <vector>
+#include "exception.h"
+
+class ShaderDesc
+{
+public:
+	ShaderDesc(Microsoft::WRL::ComPtr<ID3DBlob> blob);
+
+
+private:
+
+	std::vector<std::string> inputParams;
+
+};
