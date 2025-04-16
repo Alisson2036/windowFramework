910a6f256f081cc3233dc7d96ff69d0391eb4b51
Alisson <alissonparizoto@gmail.com>
2025-04-15 21:04:23 -0300

    Filtro anisotropico

--- windowFramework/sampler.cpp
+++ windowFramework/sampler.cpp
@@ -6,7 +6,7 @@ void Sampler::create(bool isAntialiased)
 	
 	D3D11_SAMPLER_DESC sampDesc = {};
 	if(isAntialiased)
-		sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_LINEAR;
+		sampDesc.Filter = D3D11_FILTER_ANISOTROPIC;
 	else
 		sampDesc.Filter = D3D11_FILTER_MIN_MAG_MIP_POINT;
 	//linear deixa mais suavizado, para um estilo mais
@@ -15,6 +15,7 @@ void Sampler::create(bool isAntialiased)
 	sampDesc.AddressU = D3D11_TEXTURE_ADDRESS_WRAP;
 	sampDesc.AddressV = D3D11_TEXTURE_ADDRESS_WRAP;
 	sampDesc.AddressW = D3D11_TEXTURE_ADDRESS_WRAP;
+	sampDesc.MaxAnisotropy = D3D11_MAX_MAXANISOTROPY;
 	sampDesc.MipLODBias = 0.0f;
 	sampDesc.MinLOD = 0.0f;
 	sampDesc.MaxLOD = D3D11_FLOAT32_MAX;
