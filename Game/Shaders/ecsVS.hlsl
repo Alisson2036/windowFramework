9bb2e2b471c703d16d71222b33864044426fb7ba
Alisson <alissonparizoto@gmail.com>
2026-01-03 03:46:15 -0300

    Corrigindo erros no shader code

--- Game/Shaders/ecsVS.hlsl
+++ Game/Shaders/ecsVS.hlsl
@@ -17,10 +17,6 @@ struct VS_Output
     float4 shadowPos : shadowPos;
 };
 
-cbuffer buff : register(b0)
-{
-    matrix mat;
-};
 cbuffer projectionMatrix : register(b1)
 {
     matrix projectionMat;
@@ -43,25 +39,16 @@ VS_Output main(VS_Input input)
     //matriz final
     matrix a = mul(projectionMat, tMat);
     matrix c = mul(lightMat, tMat);
-    
-    output.position = mul(float4(pos, 1.0f), transpose(tMat));
-    //output.position += float4(instancesPos[input.instance], 0.0);
-    output.position = mul(output.position, transpose(projectionMat));
-    
-    
-    output.shadowPos = mul(float4(pos, 1.0f), transpose(tMat));
-    //output.shadowPos += float4(instancesPos[input.instance], 0.0);
-    output.shadowPos = mul(output.shadowPos, transpose(lightMat));
-    
-    //output.shadowPos = mul(float4(pos, 1.0f), transpose(c));
-    
+    output.position = mul(float4(input.pos, 1.0f), transpose(a));
+    output.shadowPos = mul(float4(input.pos, 1.0f), transpose(c));
+
 
     float3x3 b = transpose((float3x3) tMat);
     output.tex = input.tex;
     output.normals = mul(input.normals, b);
     output.tangents = mul(input.tangent, b);
 
-    output.vertexPos = (float3) mul(float4(pos, 1.0f), transpose(mat));
+    output.vertexPos = (float3) mul(float4(input.pos, 1.0f), transpose(tMat));
     
 
     return output;
